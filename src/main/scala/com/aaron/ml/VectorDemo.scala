b6c4473d1b14b418533161225f4db043c5eb7445
fenghaixin <457623735@qq.com>
2017-06-07 00:31:38 +0800

    spark ml 相关性分析

--- src/main/scala/com/aaron/ml/VectorDemo.scala
+++ src/main/scala/com/aaron/ml/VectorDemo.scala
@@ -25,7 +25,6 @@ object VectorDemo
           * 第一个参数表示向量的长度
           * 第二个参数表示向量中不为0的元素的索引，从0开始计算
           * 第三个参数表示向量中不为0的元素的值，与第二个参数中的索引是一一对应的
-          */
         val sparseVector = Vectors.sparse(11, Array(1, 4, 8), Array(2, 1, 9))
 
         val sparkContext: SparkContext = SparkContextHelper.getSparkContext("local[2]", "mlLibDemo")
@@ -40,10 +39,25 @@ object VectorDemo
         println("最小值：" + result.min)
         println("方差：" + result.variance)
         println("非0个数：" + result.numNonzeros)
-        /**
+        *
           * compressed方法作用于一个稠密型向量的表示法时，会压缩为稀疏表示法
-          */
         println(denseVector.compressed)
-        println(sparseVector.asML)
+        println(sparseVector.asML)*/
+
+        relationAnalyze()
+    }
+
+
+    def relationAnalyze(): Unit =
+    {
+        val sparkContext: SparkContext = SparkContextHelper.getSparkContext("local[2]", "mlLibDemo")
+
+        val x: RDD[Double] = sparkContext.makeRDD((2 to 20 by 2).map(_.toDouble))
+
+        val y: RDD[Double] = sparkContext.makeRDD((20 to 38 by 2).map(_.toDouble))
+
+        val relation: Double = Statistics.corr(x, y, "pearson")
+
+        println("向量x和y的线性相关性：" + relation)
     }
 }
