c790bfe574b8c710b19883e46e8a7255aa8fd4ab
albert.feng <albert.feng@pkfare.com>
2018-01-25 14:26:39 +0800

    java spi机制

---
+++ src/main/java/com/aaron/spi/FileSearch.java
@@ -0,0 +1,13 @@
+package com.aaron.spi;
+
+/**
+ * 基于spi，实现服务机制
+ *
+ * @author FengHaixin
+ * @description 一句话描述该文件的用途
+ * @date 2018/1/5
+ */
+public interface FileSearch {
+
+    void searchFile();
+}
