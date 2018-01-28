816510bbd94802357e4a281c3428883c39901840
Aaron <457623735@Qq.com>
2018-01-28 14:40:37 +0800

    线性回归模型

--- src/main/scala/com/aaron/ml/LinearRegressionSample.scala
+++ src/main/scala/com/aaron/ml/LinearRegressionSample.scala
@@ -2,6 +2,7 @@ package com.aaron.ml
 
 import com.aaron.bigdata.SparkContextHelper
 import org.apache.spark.ml.regression.{LinearRegression, LinearRegressionModel}
+import org.apache.spark.rdd.RDD
 
 /**
   * 线性回归模型： y = ax + b，向量表现形式，a为行向量，x为列向量，b为残差
@@ -16,14 +17,24 @@ object LinearRegressionSample
     def main(args: Array[String]): Unit =
     {
 
-        SparkContextHelper.getSparkSession(SparkContextHelper.LOCAL_MODEL, "LinearRegressionSample")
+        val sparkSession = SparkContextHelper.getSparkSession(SparkContextHelper.LOCAL_MODEL, "LinearRegressionSample")
+
+
+        val rdd: RDD[Array[Double]] = sparkSession.sparkContext.textFile("spark-warehouse/linearRegression.txt").map(line => line.split(" ").map(_.toDouble))
+
+
+        //implicit  val encoder: Encoder = new ExpressionEncoder()
+
+        val dataSet = sparkSession.createDataset(rdd)
 
         val model: LinearRegression = new LinearRegression()
 
-        // LinearRegression.
+        model.setMaxIter(10)
 
-        LinearRegressionModel.load("spark-warehouse/linearRegression.txt")
 
+        val regressionModel: LinearRegressionModel = model.fit(dataSet)
+
+        println(regressionModel.coefficients)
     }
 
 }
