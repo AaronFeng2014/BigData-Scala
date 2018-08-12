7ba04a1fa081a2debda69326913696e675248e76
Aaron <457623735@Qq.com>
2018-08-13 00:38:53 +0800

    文本分析

---
+++ src/main/scala/com/aaron/ml/text/TextAnalysisSample.scala
@@ -0,0 +1,14 @@
+package com.aaron.ml.text
+
+/**
+  * @description 一句话描述该文件的用途
+  * @author FengHaixin
+  * @date 2018-08-12
+  */
+object TextAnalysisSample {
+
+  def main(args: Array[String]): Unit = {
+    println("this is a test")
+  }
+
+}
